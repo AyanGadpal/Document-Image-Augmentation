056eff6924771d07435947dc267add23895436b6
Ayan Naresh Gadpal <ayangadpal2@gmail.com>
2020-05-02 18:42:27 +0530

    Update DocumentAugmention.py

--- DocumentAugmention/DocumentAugmention.py
+++ DocumentAugmention/DocumentAugmention.py
@@ -2,30 +2,39 @@ from random import randint
 import cv2
 import numpy as np
 
+# Apply basic Thresholding
 def basicTransform(img):
 	_, mask = cv2.threshold(img,220,255,cv2.THRESH_BINARY_INV)
 	img = cv2.bitwise_not(mask)
 	return img
 
+# Change The background color (currently assumed white) to random color
 def changeColor(image):
+  # Color list to select color from
   colorList = [[228,238,240],[148,236,255],[174,174,255],[103,255,220],[124,229,176],[231,216,180]]
+  # Random index
   randomColor = randint(0,5)
   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
   ret, mask = cv2.threshold(gray, 0, 255,cv2.THRESH_BINARY_INV |cv2.THRESH_OTSU)
+  # Making a copy
   coloredImage = image.copy()
+  # Changing color
   coloredImage[mask == 0] = colorList[randomColor]
   return coloredImage
 
+# Increase the Brightness
 def BrightnessUp(image,alpha=50.0):
   alpha = float(alpha)
   image = cv2.add(image,np.array([alpha]))
   return image
 
+# Decrease the Brightness
 def BrightnessDown(image,alpha=50.0):
   alpha = float(alpha)
   image = cv2.subtract(image,np.array([alpha]))
   return image
 
+# Smudging
 def Smudge(image):
   b,g,r = cv2.split(image)
   
@@ -57,6 +66,7 @@ def Smudge(image):
 
   return SmudgedImage
 
+# Dialate
 def Dialate(image,kernal=np.ones((2,2),np.uint8)):
   _, mask = cv2.threshold(image,220,255,cv2.THRESH_BINARY_INV)
   dst = cv2.dilate(mask,kernal,iterations = 1)
